d0f873579a79be4a474088d3ffe9e0672be3188b
coo1white <126941599+coo1white@users.noreply.github.com>
2026-05-02 09:28:30 +0700

    Initial Commit

---
+++ naive/naiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  naiveApp.swift
+//  naive
+//
+//  Created by Nick Bai on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct naiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
